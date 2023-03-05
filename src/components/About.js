49daa91f617ece5197061e92ca264d1ad53149d1
Yves-Henry Moraille <yves-henrymoraille@Yves-Henrys-Air.lan>
2023-03-05 15:29:17 -0500

    added color in footer in the about js file

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,8 @@ export default function About() {
 <div class='about'>
     <h2> About Me</h2>
     <img className='Picture' src={Picture}></img>
-    <li>
+    <hr />
+    <li class='intro'>
 First and foremost I am a devoted father who is looking to thrive in every aspect of life. I am currently changing career from being a Truck Driver into A Full Stack Software Engineer.  I did a Full Time bootcamp at UNCC.
 At UNCC I was able to learn diffenrent technologies in Coding.  For example: Vanilla JavaScript, Node.Js, React.js, Express.js.  I am always ready to learn new technologies when it comes to Coding. I am a fast learner and a Team player.
     </li>
